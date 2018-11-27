dfb9f8c5d6ce3e462bb6a580458269d5c5371af8
seceretrui <35143675+seceretrui@users.noreply.github.com>
2018-11-27 23:21:51 +0800

    Create 19033.js
    indo about cqgc

---
+++ univ/19033.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "重庆工程学院",
+    id: "19033",
+    link: [ {
+        name: "重工首页",
+        url: "http://www.cqie.edu.cn/"
+    }, {
+        name: "就业管理",
+        url: "http://jy.cqie.edu.cn/"
+    }, {
+        name: "教务管理",
+        url: "http://jw.cqie.edu.cn/"
+    }, {
+        name: "学生综合管理"，
+        url: "http://xs.cqie.edu.cn/"
+    }, {
+        name: "网络教学"，
+        url: "http://wj.cqie.edu.cn/meol/homepage/common/"
+    }, {
+        name: "图书馆藏",
+        url: "http://lib.cqie.edu.cn/cmsController.do?goPage&page=index&siteCode=portal"
+    }, {
+        name: "重工邮箱",
+        url: "http://mail.cqie.edu.cn/"
+    } ]
+};
